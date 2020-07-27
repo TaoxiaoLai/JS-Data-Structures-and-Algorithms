8dc026a57d22f0f513d5ba40616358f62123e44a
laijintao <18811526955@163.com>
2020-07-27 10:52:21 +0800

    基础动态规划

---
+++ code/动态规划/climbStairs.js
@@ -0,0 +1,48 @@
+// 动态规划————爬楼梯问题
+// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+// 示例：
+// 输入：3
+// 输出：3
+// 解释：有三种方法可以爬到楼顶。
+// 1 阶 + 1 阶 + 1 阶
+// 1 阶 + 2 阶
+// 2 阶 + 1 阶
+
+// 直接递归解法
+function climbStairs1(n) {
+    if (n === 1) {
+        return 1
+    }
+    if (n === 2) {
+        return 2
+    }
+    return climbStairs(n-1) + climbStairs(n-2)
+}
+// console.log(climbStairs1(10))
+
+// 记忆化搜索优化
+function climbStairs2(n) {
+    let f = []
+    if (n === 1) {
+        return 1
+    }
+    if (n === 2) {
+        return 2
+    }
+    if (f[n] === undefined) f[n] = climbStairs2(n-1) + climbStairs2(n-2)
+    return f[n]
+}
+// console.log(climbStairs2(20))
+
+// 动态规划优化
+function climbStairs3(n) {
+    let f = []
+    f[1] = 1
+    f[2] = 2
+    for (let i=3; i<=n; i++) {
+        f[i] = f[i-2] + f[i-1] 
+    }
+    return f[n]
+}
+console.log(climbStairs3(20))
---
+++ code/动态规划/coinChange.js
@@ -0,0 +1,25 @@
+// “最值”型问题：找硬币
+// 题目描述：给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+// 如果没有任何一种硬币组合能组成总金额，返回 -1。
+// 示例：
+// 输入: coins = [1, 2, 5], amount = 11
+// 输出: 3
+// 解释: 11 = 5 + 5 + 1
+
+function coinChange(coins, amount) {
+    let f = []
+    f[0] = 0
+    for (let i=1; i<=amount; i++) {
+        f[i] = Infinity
+        for (let j=0; j<coins.length; j++) {
+            if (i-coins[j] >= 0) {
+                f[i] = Math.min(f[i], f[i-coins[j]]+1)
+            }
+        }
+    }
+    if(f[amount] === Infinity) {
+        return -1
+    }
+    return f[amount]
+}
+console.log(coinChange([1,2,5], 11))
